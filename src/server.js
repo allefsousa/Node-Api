01375107470e097a53746f9e4f216801fdd0a140
Allef Sousa <allefsousa_1@hotmail.com>
2019-11-10 23:53:01 -0300

    Separando as rotas

--- src/server.js
+++ src/server.js
@@ -1,8 +1,17 @@
 const express = require("express")
+const routes = require('./routes')
 
 const app = express();
 
-app.get('/',(req,res) =>{
-    return res.json({message: "Hello"});
-});
+// GET, POST, DELETE,PUT
+
+// req.query = Acessar a query params (Para filtros)
+// req.params = Acessar route params (Para edição e delete)
+// req.body = Acessar corpo da requisição (Para criação e edição)
+
+app.use(express.json)
+app.use(routes)
+
+
+
 app.listen(3000);
